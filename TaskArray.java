d27cb846e4743007e30b6f4b9b48bd411005a015
Eugene Papkou <30703998+EugenePapkou@users.noreply.github.com>
2017-08-23 00:14:57 +0300

    Update TaskArray.java

--- TaskArray.java
+++ TaskArray.java
@@ -12,55 +12,7 @@ public class TaskArray
 {          
     public static void main(String[] args) 
     {                
-        ArrayList a = new ArrayList();
-        ArrayList b = new ArrayList();
-        a.add(1);
-        a.add(3);
-        a.add(555);
-        b.add(2);        
-        b.add(6);
-        b.add(888);
-        TaskArray obj = new TaskArray();
-        obj.merge(a, b);       
+        Merge obj = new Merge();        
+        System.out.println(obj.merge(obj.a, obj.b)); 
     }
-    
-    void merge(ArrayList a, ArrayList b)
-    {        
-        ArrayList c = new ArrayList();
-        int i = 0;
-        int k = 0;
-        c.addAll(a);
-        a.clear();
-        while((i < c.size()) && ((int)c.get(i) <= (int)b.get(k)))
-        {
-            a.add(c.get(i));
-            i++;
-            if (i == c.size())
-            {
-                while(k < c.size())
-                {
-                    a.add(b.get(k));
-                    k++;
-                }
-            }
-            else
-            {
-                while((k < c.size()) && ((int)c.get(i) > (int)b.get(k)))
-                {
-                    a.add(b.get(k));
-                    k++;
-                }
-                if (k == c.size())
-                {
-                    while(i < c.size())
-                    {
-                        a.add(c.get(i));
-                        i++;
-                    }
-                }
-            }
-        }    
-        System.out.println(a);
-    }
-    
 }
